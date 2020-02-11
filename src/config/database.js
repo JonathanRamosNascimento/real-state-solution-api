46664d37d2ae8ea7b0f2dfa79cf6c9e18d36164c
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-11 16:58:13 -0300

    Altenrando o .env

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose').set('useCreateIndex', true);
 require('dotenv/config');
 
 
-// mongoose.connect('mongodb://'+process.env.DB_HOST+':27017/'+process.env.DB_NAME, {
-mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds163517.mlab.com:63517/heroku_lrxcr96b', {
+//mongoose.connect('mongodb://'+process.env.DB_HOST+':27017/'+process.env.DB_NAME, {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
