3dbc7d0d4fa15072443f5229bf5002071cb7c009
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-11 16:32:05 -0300

    usando banco de dados do heroku

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose').set('useCreateIndex', true);
 require('dotenv/config');
 
 
-mongoose.connect('mongodb://'+process.env.DB_HOST+':27017/'+process.env.DB_NAME, {
+// mongoose.connect('mongodb://'+process.env.DB_HOST+':27017/'+process.env.DB_NAME, {
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds163517.mlab.com:63517/heroku_lrxcr96b', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
