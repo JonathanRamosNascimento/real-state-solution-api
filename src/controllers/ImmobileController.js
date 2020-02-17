adff020f3192b65fa27f4323d61af04af76669ac
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2020-02-16 21:27:37 -0300

    Refazendo a roda de imóveis, buscando todos se não houver nenhum filtro nos parametros

--- src/controllers/ImmobileController.js
+++ src/controllers/ImmobileController.js
@@ -3,10 +3,14 @@ const Immobile = require('../models/Immobile');
 module.exports = {
   async index(req, res) {
     try {
-      const { tamanho, quarto, suite, banheiro, piso, varanda, garagem, piscina, moveisImbutidos, bairro, cidade, estado } = req.query;
-      const immobiles = await Immobile.find({ $and: [{ tamanho: { $gte: tamanho } }, { quarto }, { suite }, { banheiro }, { piso }, { varanda }, { garagem }, { piscina }, { moveisImbutidos }, { bairro }, { cidade }, { estado }] });
+      if (req.query.tamanho) {
+        const { tamanho, quarto, suite, banheiro, piso, varanda, garagem, piscina, moveisImbutidos, bairro, cidade, estado } = req.query;
+        const immobiles = await Immobile.find({ $and: [{ tamanho: { $gte: tamanho } }, { quarto }, { suite }, { banheiro }, { piso }, { varanda }, { garagem }, { piscina }, { moveisImbutidos }, { bairro }, { cidade }, { estado }] });
 
-      return res.json(immobiles);
+        return res.json(immobiles);
+      }
+      const resposta = await Immobile.find();
+      return res.json(resposta);
     } catch (err) {
       return res.status(400).send({ error: err });
     }
@@ -18,7 +22,7 @@ module.exports = {
 
       const immobile = await Immobile.findById(id);
       const dicas = await Immobile.find({ bairro: immobile.bairro });
-      
+
       return res.send({ imovel: immobile, dicas });
     } catch (error) {
       return res.status(400).send({ error: err });
